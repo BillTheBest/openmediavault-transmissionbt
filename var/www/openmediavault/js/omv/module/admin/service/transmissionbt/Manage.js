1a7be17ef2da9937a8a62b4d9b50a144bc8fef3f
Aaron Murray <aaron@omv-extras.org>
2013-09-24 16:53:14 -0500

    Fix copyrights

--- var/www/openmediavault/js/omv/module/admin/service/transmissionbt/Manage.js
+++ var/www/openmediavault/js/omv/module/admin/service/transmissionbt/Manage.js
@@ -1,5 +1,6 @@
 /**
  * Copyright (C) 2013 OpenMediaVault Plugin Developers
+ * Copyright (c) 2012 Marcel Beck
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
